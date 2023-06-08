12196e9f31063c20dade871eb50c7c703547910c
Revku <revkuu@gmail.com>
2023-06-08 18:34:44 +0200

    Add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from 'src/pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+]);
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 };
 
 export default App;
