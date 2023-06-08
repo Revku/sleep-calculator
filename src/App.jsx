ded2e9454195d49186bd1c6031e92a8a66cd77da
Revku <revkuu@gmail.com>
2023-06-08 19:11:21 +0200

    Add google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ReactGA from 'react-ga4';
 import Home from 'src/pages/Home';
+
+// Google Analytics initialization
+ReactGA.initialize('G-3NQ8HBSZD6');
 
 const router = createBrowserRouter([
   {
